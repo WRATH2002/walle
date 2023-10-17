59965a13faffe06726fb50860a53a427d078fe5a
Himadri Purkait <purkaithimadricollege@gmail.com>
2023-10-17 19:41:36 +0530

    4th commit

--- src/firebase.js
+++ src/firebase.js
@@ -7,11 +7,11 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: "walle-7a034.firebaseapp.com",
+  projectId: "walle-7a034",
+  storageBucket: "walle-7a034.appspot.com",
+  messagingSenderId: "176206594273",
+  appId: "1:176206594273:web:3351e09ec96b448d11f7a0",
 };
 
 // Initialize Firebase
